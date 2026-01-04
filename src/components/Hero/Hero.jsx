b5baac33621721aa1dbfe57ce7b5368d9d5af49c
maisu764 <maisu764@gmail.com>
2026-01-04 15:26:22 +0500

    Resolve merge conflict in Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react'
-<<<<<<< HEAD
-import banners from '../data/bannerImages' // your array of images
-=======
 import banners from '../../data/bannerImages' // your array of images
->>>>>>> a652b11fa0d6a3e443a56bd6d7d53eeb6c490a81
 
 const Hero = () => {
   const [current, setCurrent] = useState(0)
@@ -26,18 +22,14 @@ const Hero = () => {
 
   return (
     <section className="pt-2 mt-[10vh]">
-      {/* Center container with max width and px-10 */}
       <div className="max-w-[1200px] mx-auto px-10">
         <div className="relative w-full h-[400px] md:h-[500px] overflow-hidden rounded-lg">
-
-          {/* Banner Image */}
           <img
             src={banners[current]}
             alt="Banner"
-            className="w-full h-full object-contain transition-all duration-700 "
+            className="w-full h-full object-contain transition-all duration-700"
           />
 
-          {/* Left Arrow */}
           <button
             onClick={prevSlide}
             className="absolute top-1/2 left-4 -translate-y-1/2 bg-black/50 text-white p-3 rounded-full hover:bg-black"
@@ -45,14 +37,12 @@ const Hero = () => {
             ❮
           </button>
 
-          {/* Right Arrow */}
           <button
             onClick={nextSlide}
             className="absolute top-1/2 right-4 -translate-y-1/2 bg-black/50 text-white p-3 rounded-full hover:bg-black"
           >
             ❯
           </button>
-
         </div>
       </div>
     </section>
