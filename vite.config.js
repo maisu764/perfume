cc48815c2acea8d87713cf6227d9a4f460925d35
maisu764 <maisu764@gmail.com>
2026-01-04 15:14:48 +0500

    Fix vite.config.js merge conflict

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   base: "/perfume/" // MUST match repo name exactly (lowercase)
-});
->>>>>>> a652b11fa0d6a3e443a56bd6d7d53eeb6c490a81
+})
