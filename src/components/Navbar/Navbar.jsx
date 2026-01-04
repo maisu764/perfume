4d8222db5c7eb63d5a193b59af5e26472ef4bf43
maisu764 <maisu764@gmail.com>
2026-01-04 15:24:24 +0500

    Resolve merge conflict in Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,16 @@
 import React, { useEffect, useState } from "react";
-<<<<<<< HEAD
-import maisu from "../../../public/maisu.png";
-=======
-import maisu from "../../assets/maisu.png";
->>>>>>> a652b11fa0d6a3e443a56bd6d7d53eeb6c490a81
+import maisu from "../../assets/maisu.png"; // choose the correct path
 import { IoMdHeart } from "react-icons/io";
 import { RiShoppingBasketLine } from "react-icons/ri";
 import { IoIosSearch } from "react-icons/io";
 
-<<<<<<< HEAD
 const Navbar = ({ handleScroll, setSearchTerm, isScrolled, handlePanel, totalItems, wishlist }) => {
-
-
-=======
-const Navbar = ({ handleScroll, setSearchTerm, isScrolled }) => {
-
-   
->>>>>>> a652b11fa0d6a3e443a56bd6d7d53eeb6c490a81
 
     return (
         <header className={`bg-white fixed top-0 left-0 right-0 z-50 ${isScrolled ? 'shadow-lg' : ''}`}>
             <nav className="max-w-[1200px] mx-auto px-10 h-[10vh] flex items-center justify-between">
                 <a href="#">
                     <img src={maisu} alt="logo" className="w-10 h-10" />
-
                 </a>
 
                 {/* Desktop Menu Example */}
@@ -37,7 +24,7 @@ const Navbar = ({ handleScroll, setSearchTerm, isScrolled }) => {
                 {/* Nav Action */}
                 <div className="flex items-center gap-x-5">
                     {/* Input Field */}
-                    <div className="flex p-1  border-2 rounded-full">
+                    <div className="flex p-1 border-2 rounded-full">
                         <input type="text" name="text" id="text" placeholder="Search...." autoComplete="off"
                             className="flex-1 h-[3vh] px-3 pt-2 focus:outline-none w-40"
                             onFocus={handleScroll}
@@ -47,11 +34,9 @@ const Navbar = ({ handleScroll, setSearchTerm, isScrolled }) => {
                         </button>
                     </div>
 
-<<<<<<< HEAD
                     <button className="text-[1.7rem] text-zinc-800 relative cursor-pointer">
                         <IoMdHeart className="text-red-600 text-3xl cursor-pointer" onClick={() => handlePanel('wishlist')} />
-                        {
-                            wishlist.length > 0 &&
+                        {wishlist.length > 0 &&
                             <span className="flex justify-center items-center bg-black text-white w-5 h-5 rounded-full text-[12px] absolute top-4 right-3 border-2 border-white">
                                 {wishlist.length}
                             </span>
@@ -60,25 +45,12 @@ const Navbar = ({ handleScroll, setSearchTerm, isScrolled }) => {
 
                     <button className="text-indigo-600 text-3xl cursor-pointer relative " onClick={() => handlePanel('cart')}>
                         <RiShoppingBasketLine />
-                        {
-                            totalItems > 0 &&
+                        {totalItems > 0 &&
                             <span className="flex justify-center items-center bg-black text-white w-5 h-5 rounded-full text-[12px] absolute top-4 right-4 border-2 border-white">
                                 {totalItems}
                             </span>
                         }
-
-
-                       
                     </button>
-=======
-                    <a href="#">
-                        <IoMdHeart className="text-red-600 text-2xl" />
-                    </a>
-
-                    <a href="#">
-                        <RiShoppingBasketLine className="text-indigo-600 text-2xl" />
-                    </a>
->>>>>>> a652b11fa0d6a3e443a56bd6d7d53eeb6c490a81
                 </div>
             </nav>
         </header>
